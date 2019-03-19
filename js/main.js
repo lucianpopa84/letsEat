c2749c6103721adda2f21ab15f78d1a499b763cb
lucianpopa84 <lucianpopa84@gmail.com>
2019-03-19 23:30:52 +0200

    Added geolocation
    and fixed some styling

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var detectedPlaceText = document.getElementById("detectedPlace");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(showPosition, showError);
+    } else {
+        detectedPlaceText.innerHTML = "Geolocation <br> not supported.";
+    }
+}
+
+function showPosition(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    codeLatLng(lat, lng);
+
+}
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            detectedPlaceText.innerHTML = "User denied <br>Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            detectedPlaceText.innerHTML = "Location <br>is unavailable."
+            break;
+        case error.TIMEOUT:
+            detectedPlaceText.innerHTML = "Get location <br>timed out."
+            break;
+    }
+}
+
+function codeLatLng(lat, lng) {
+
+    var latlng = new google.maps.LatLng(lat, lng);
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            // console.log(results)
+            if (results) {
+                detectedPlaceText.innerHTML = "Detected location:<br>" + results[0].address_components[2].long_name;
+            } else {
+                console.log("No results found");
+            }
+        } else {
+            console.log("Geocoder failed due to: " + status);
+        }
+    });
+}
+
+getLocation();
