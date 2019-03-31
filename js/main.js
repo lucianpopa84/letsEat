9063272b4f52349749cfe3a6e068327456a52bbe
lucianpopa84 <lucianpopa84@gmail.com>
2019-03-31 23:27:03 +0300

    fix geolocation, search for address components in all result array

--- js/main.js
+++ js/main.js
@@ -15,8 +15,10 @@ function getLocation() {
 }
 
 function showPosition(position) {
+    // get latitude and longitude
     var lat = position.coords.latitude;
     var lng = position.coords.longitude;
+    // get location components (city, street, etc.) based on latitude and longitude
     codeLatLng(lat, lng);
 }
 
@@ -39,23 +41,52 @@ function codeLatLng(lat, lng) {
     geocoder.geocode({ 'latLng': latlng }, function (results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
             if (results) {
-                if (results[0].address_components[0].types.includes("street_number")
-                    && results[0].address_components[2].types.includes("locality")
-                    && results[0].address_components[1].types.includes("route")) {
-                    var streetNumber = results[0].address_components[0].long_name;
-                    var locality = results[0].address_components[2].long_name;
-                    localStorage.setItem("locality", locality);
-                    var street = results[0].address_components[1].long_name;
-                    if (street.startsWith("Strada")) {
-                        street = `${street.substring(6, street.length)} street`;
+                for (let result of results) {
+                    // search for street number
+                    for (let address of result.address_components) {
+                        if (address.types.includes("street_number")) {
+                            console.log("Street number results:", address.long_name);
+                            var streetNumber = address.long_name;
+                            break;
+                        }
                     }
-                    detectedPlaceText.innerHTML = `Detected location:<br>
-                    ${streetNumber} ${street}, ${locality}`;
-                } else {
-                    detectedPlaceText.innerHTML = `Detected location:<br>
-                    ${results[0].formatted_address.split(",")[0]}, ${results[0].formatted_address.split(",")[1]}`;
-                    var locality = results[0].formatted_address.split(",")[1];
-                    localStorage.setItem("locality", locality);
+                    // search for locality (city)
+                    for (let address of result.address_components) {
+                        if (address.types.includes("locality")) {
+                            console.log("Locality results:", address.long_name);
+                            var locality = address.long_name;
+                            localStorage.setItem("locality", locality);
+                            break;
+                        }
+                    }
+                    // search for route (street)
+                    for (let address of result.address_components) {
+                        if (address.types.includes("route")) {
+                            console.log("Route results:", address.long_name);
+                            var street = address.long_name;
+                            if ( document.documentElement.lang.toLowerCase() === "en-us" ) {
+                                // cut "Strada" from street and add "street" in the end, if language is english
+                                if (street.startsWith("Strada")) {
+                                    street = `${street.substring(6, street.length)} street`;
+                                }
+                                // cut "Bulevardul" from street and add "boulevard" in the end, if language is english
+                                if (street.startsWith("Bulevardul")) {
+                                    street = `${street.substring(10, street.length)} boulevard`;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                    // break loop if all address components found
+                    if (streetNumber && street && locality) {
+                        detectedPlaceText.innerHTML = `Detected location:<br>
+                        ${streetNumber} ${street}, ${locality}`;
+                        break;
+                    } else {
+                        detectedPlaceText.innerHTML = `Detected location: <br>
+                        ${results[0].formatted_address.split(",")[0]}, ${results[0].formatted_address.split(",")[1]} <br>
+                        Delivery not available for your area`;
+                    }
                 }
             } else {
                 console.log("No results found");
@@ -106,7 +137,6 @@ fetch(jsonDataUrl)
         return response.json();
     })
     .then(function (data) {
-        // console.log(data);
         // localStorage.setItem("restaurantsData", JSON.stringify(data));
         restaurantsData = data;
     });
@@ -167,7 +197,7 @@ foodTypeListInput.onclick = function () {
 function generateRestaurantCards(food) {
     if (restaurantsData) {
         let htmlContent = "";
-        // retrieve city from local storage or city input
+        // retrieve city from local storage or manual city input
         let city = "";
         let cityManual = localStorage.getItem("localityManual");
         if (cityManual) {
@@ -176,42 +206,42 @@ function generateRestaurantCards(food) {
             city = localStorage.getItem("locality");
         }
         console.log("city = ", city);
-        for (let i = 0; i < restaurantsData.length; i++) {
-            if (restaurantsData[i].foodType.includes(food)) {
-                if (restaurantsData[i].location == city) {
+        for (let restaurantData of restaurantsData) {
+            if (restaurantData.foodType.includes(food)) {
+                if (restaurantData.location == city) {
                     htmlContent += `
-                    <div class="restaurantCard" id="${restaurantsData[i].id}" onclick="displayFood(this)"> 
+                    <div class="restaurantCard" id="${restaurantData.id}" onclick="displayFood(this)"> 
                         <div class="row">
                             <div class="col-3">
                                 <div class="card-image">
-                                    <img src="${restaurantsData[i].imageSrc}" alt="${restaurantsData[i].imageAlt}">
+                                    <img src="${restaurantData.imageSrc}" alt="${restaurantData.imageAlt}">
                                 </div>
                             </div>
                             <div class="col-4">
                                 <div class="card-delivery">
-                                    <h4>${restaurantsData[i].name}</h4>`;
+                                    <h4>${restaurantData.name}</h4>`;
                     let currentDate = new Date();
                     let currentHour = currentDate.getHours();
-                    if (currentHour < restaurantsData[i].closeHour && currentHour >= restaurantsData[i].operHour) {
-                        htmlContent += `<p> <i class="fas fa-truck"></i> <span class="deliveryTime"> ${restaurantsData[i].deliveryTime} min.</span> </p>`;
+                    if (currentHour < restaurantData.closeHour && currentHour >= restaurantData.operHour) {
+                        htmlContent += `<p> <i class="fas fa-truck"></i> <span class="deliveryTime"> ${restaurantData.deliveryTime} min.</span> </p>`;
                     } else {
                         htmlContent += `<p> <i class="fas fa-truck"></i> <span class="deliveryTime"> closed </span> </p>`;
                     }
                     htmlContent += `</div>
                                 <div class="card-rating">`;
-                    for (let j = 0; j < restaurantsData[i].rating; j++) {
+                    for (let j = 0; j < restaurantData.rating; j++) {
                         htmlContent += `<span class="fa fa-star checked"></span>`;
                     };
-                    for (let j = 0; j < 5 - restaurantsData[i].rating; j++) {
+                    for (let j = 0; j < 5 - restaurantData.rating; j++) {
                         htmlContent += `<span class="fa fa-star"></span>`;
                     };
                     htmlContent += `</div>
                             </div>
                             <div class="col-4">
                                 <div class="card-pricing">
-                                    <p> From: ${restaurantsData[i].pricingFrom} RON </p>
-                                    <p> To: ${restaurantsData[i].pricingTo} RON </p>
-                                    <p> Min: ${restaurantsData[i].pricingMin} RON </p>
+                                    <p> From: ${restaurantData.pricingFrom} RON </p>
+                                    <p> To: ${restaurantData.pricingTo} RON </p>
+                                    <p> Min: ${restaurantData.pricingMin} RON </p>
                                 </div>
                             </div>
                         </div>
@@ -239,7 +269,7 @@ document.querySelector("#topMenu").addEventListener("click", function () {
 
 // remove selected food quick link
 function removeSelectedQuickLink() {
-    for (var i = 0; i < quickLink.length; i++) {
+    for (let i = 0; i < quickLink.length; i++) {
         var current = document.querySelectorAll(".active");
         if (current.length > 0) {
             current[0].className = current[0].className.replace(" active", "");
@@ -252,16 +282,36 @@ function displayFood(restaurant) {
     console.log("selected restaurant: ", restaurant.id);
     // remove other restaurant cards
     if (restaurantList.hasChildNodes()) {
-        let restaurantCard = restaurantList.querySelectorAll(".restaurantCard");
-        for (let i = 0; i < restaurantCard.length; i++) {
-            if (restaurantCard[i].id != restaurant.id) {
-                restaurantList.removeChild(restaurantCard[i]);
+        let restaurantCards = restaurantList.querySelectorAll(".restaurantCard");
+        for (let restaurantCard of restaurantCards) {
+            if (restaurantCard.id != restaurant.id) {
+                restaurantList.removeChild(restaurantCard);
             }
         }
     }
-    // remove quick grid
+    // remove quick grid and food search filter
+    removeElementById("foodQuickSearchGrid");
+    removeElementByClass("foodSearchFilter");
+    // display food - work in progress
 }
 
+// remove element by id
+function removeElementById(id) {
+    let element = document.querySelector(`#${id}`);
+    if (element) {
+        element.parentNode.removeChild(element);
+    }
+    return false;
+}
+
+// remove element by class name
+function removeElementByClass(className) {
+    let element = document.querySelector(`.${className}`);
+    if (element) {
+        element.parentNode.removeChild(element);
+    }
+    return false;
+}
 
 
 
