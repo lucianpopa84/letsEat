a2e18f20911801da1e9b0742571dfe0f1d043415
lucianpopa84 <lucianpopa84@gmail.com>
2019-08-08 00:16:36 +0300

    modified useGeocoder.js to search street, street number and city in location results

--- src/useGeocoder.js
+++ src/useGeocoder.js
@@ -14,6 +14,9 @@ function useGeocoder() {
    const [longitude, setLongitude] = useState(23.8167439);
 
    function geoFindMe() {
+      let streetNumber = "";
+      let locality = "";
+      let street = "";
       function success(position) {
          let lat = position.coords.latitude;
          let long = position.coords.longitude;
@@ -23,7 +26,73 @@ function useGeocoder() {
          // Get address from latidude & longitude.
          Geocode.fromLatLng(latitude, longitude).then(
             response => {
-               setDetectedAddress(response.results[0].formatted_address);
+               if (response.results) {
+                  for (let result of response.results) {
+                     // search for street number
+                     for (let address of result.address_components) {
+                        if (address.types.includes("street_number")) {
+                           console.log(
+                              "Street number results:",
+                              address.long_name
+                           );
+                           streetNumber = address.long_name;
+                           break;
+                        }
+                     }
+                     // search for locality (city)
+                     for (let address of result.address_components) {
+                        if (address.types.includes("locality")) {
+                           console.log("Locality results:", address.long_name);
+                           locality = address.long_name;
+                           localStorage.setItem("locality", locality);
+                           break;
+                        }
+                     }
+                     // search for route (street)
+                     for (let address of result.address_components) {
+                        if (address.types.includes("route")) {
+                           console.log("Route results:", address.long_name);
+                           street = address.long_name;
+                           if (
+                              document.documentElement.lang.toLowerCase() ===
+                              "en"
+                           ) {
+                              // cut "Strada" from street and add "street" in the end, if language is english
+                              if (street.startsWith("Strada")) {
+                                 street = `${street.substring(
+                                    6,
+                                    street.length
+                                 )} street`;
+                              }
+                              // cut "Bulevardul" from street and add "boulevard" in the end, if language is english
+                              if (street.startsWith("Bulevardul")) {
+                                 street = `${street.substring(
+                                    10,
+                                    street.length
+                                 )} boulevard`;
+                              }
+                           }
+                           break;
+                        }
+                     }
+                     // break loop if all address components found
+                     if (streetNumber && street && locality) {
+                        setDetectedAddress(
+                           `${streetNumber} ${street}, ${locality}`
+                        );
+                        // getCityId(locality);
+                        break;
+                     } else {
+                        setDetectedAddress(
+                           `${response.results[0].formatted_address.split(",")[0]},
+                            ${response.results[0].formatted_address.split(",")[1]}
+                            Delivery not available for your area`
+                        );
+                     }
+                  }
+               } else {
+                  console.log("No results found");
+               }
                setAddress(`Detected location: ${detectedAddress}`);
             },
             error => {
